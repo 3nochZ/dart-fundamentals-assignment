69716cee191a98d93fd8d5dfae5932761edf3a0f
enoch <henokzjohn@gmail.com>
2026-03-14 21:54:28 +0300

    implement findmin() and calculateSum()

--- task2/calculator_app.dart
+++ task2/calculator_app.dart
@@ -12,12 +12,42 @@ int findMax(List<int> numbers) {
 	return max;
 }
 
+int findMin(List<int> numbers) {
+  int min = numbers[0];
+
+  for (int n in numbers) {
+    if (n < min) {
+      min = n; 
+    }
+  }
+
+  return min;
+}
+
+int calculateSum(List<int> numbers) {
+  int sum = 0; 
+
+  for (int n in numbers) {
+    sum += n;
+  }
+
+  return sum;
+}
+
 // test
 void main() {
-  final numbers = <int>[34, -7, 89, 12, -45, 67, 3, 100, -2, 55];
+	final numbers = <int>[34, -7, 89, 12, -45, 67, 3, 100, -2, 55];
 
-  int max = findMax(numbers);
+	int max = findMax(numbers);
+	int min = findMin(numbers);
+	int sum = calculateSum(numbers);
 
-  print('numbers: $numbers');
-  print('maximum value : $max');
+	print('Number Analysis Results');
+	print('========================');
+	print('numbers: $numbers');
+	print('Maximum Value : $max');
+	print('Minimum Value : $min');
+	print('Sum: $sum');
 }
+
+
