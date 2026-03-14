1592530dab6ebcec9bd5be4bdbb6743e70e673f1
enoch <henokzjohn@gmail.com>
2026-03-14 22:29:03 +0300

    add sortNumbers() without .sort()

--- task2/calculator_app.dart
+++ task2/calculator_app.dart
@@ -75,6 +75,23 @@ int countNegatives(List<int> numbers) {
 	return count;
 }
 
+// bubble sort
+List<int> sortNumbers(List<int> numbers) {
+  List<int> sorted = List.from(numbers); // copy list
+
+  for (int i = 0; i < sorted.length - 1; i++) {
+    for (int j = 0; j < sorted.length - i - 1; j++) {
+      if (sorted[j] > sorted[j + 1]) {
+        int temp = sorted[j];
+        sorted[j] = sorted[j + 1];
+        sorted[j + 1] = temp;
+      }
+    }
+  }
+
+  return sorted;
+}
+
 // test
 void main() {
 	final numbers = <int>[34, -7, 89, 12, -45, 67, 3, 100, -2, 55];
